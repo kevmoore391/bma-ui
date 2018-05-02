48bcc5da6a492b316a72c6c4f51688c6768399b7
Moore <Kevin.Moore1@emc.com>
2018-05-02 11:05:09 +0100

    Remove test for H1 Title on app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,11 +20,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent)
-      .toContain('Welcome to Titan UI 2.0 seed project!');
-  }));
+  
 });
